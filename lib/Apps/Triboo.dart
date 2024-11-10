7a1e47f32a4378cc8e138130be6d3598317e4ede
EvoRaptar <santiagopalou@gmail.com>
2024-11-10 17:46:58 +0100

    Creacion de las primeras rutas de navegacion para que se mueva entre las partes, me dispongo a empezar con las views

--- lib/Apps/Triboo.dart
+++ lib/Apps/Triboo.dart
@@ -8,11 +8,17 @@ class Triboo extends StatelessWidget {
 
     // Hay que definir un array de rutas
     Map<String,Widget Function(BuildContext)> rutasNavegacion = {
+      '/SplashView':(context)=> SplashView(),
+      '/LoginView':(context)=> LoginView(),
+      '/HomeView':(context)=>  HomeView(),
+      '/MainView':(context)=> MainView(),
+      '/RegisterView':(context) => RegisterView(),
+      '/ProfileUserView':(context) => ProfileUserView(),
     };
 
     MaterialApp app = MaterialApp(title: "Triboo",
       routes: rutasNavegacion,
-      initialRoute: '/',
+      initialRoute: '/SplashView',
       debugShowCheckedModeBanner: false, // Oculta el banner de debug
     );
     return app;

663c834a3c1795526615714de1b600af4696b17d
EvoRaptar <santiagopalou@gmail.com>
2024-12-07 00:38:11 +0100

    Inserccion del Primer home donde Iran los chats, en este caso he reciclado codigo pero ahi tendria que ir los chats, por lo que hay que refactorizar codigo y dejar que te deje ver chats en vez de productos

---
+++ lib/Statics/Products.dart
@@ -0,0 +1,8 @@
+
+class Products {
+  String name;
+  double price;
+  String store;
+
+  Products({required this.name, required this.price, required this.store});
+}
--- lib/Views/HomeView.dart
+++ lib/Views/HomeView.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../Statics/Products.dart';
+import 'Customs/CustomButtonSanti.dart';
+import 'Customs/CustomDrawer.dart';
 
 class HomeView extends StatefulWidget {
   @override
@@ -9,14 +13,65 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeView extends State<HomeView> {
-  int clickContador = 0;
-  bool visible = true;
+  int _selectedIndex = 0;
 
-  void clickBtnContador() {
+  // Lista para almacenar los productos
+  List<Products> _products = [];
+
+  // Variable para alternar entre vista en lista o en celdas
+  bool isGridView = false;
+
+  // Cambiar el índice seleccionado
+  void _onItemTapped(int index) {
     setState(() {
-      clickContador++;
-      visible = !visible;
+      _selectedIndex = index;
     });
+  }
+
+  // Función para agregar productos a la lista
+  void _addProduct(Products product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
+  // Función para obtener productos desde Firestore
+  void _getProducts() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      String userId = user.uid;
+
+      try {
+        QuerySnapshot snapshot = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userId)
+            .collection('productos')
+            .orderBy('createdAt', descending: true)
+            .get();
+
+        List<Products> fetchedProducts = snapshot.docs.map((doc) {
+          return Products(
+            name: doc['name'],
+            price: doc['price'],
+            store: doc['store'],
+          );
+        }).toList();
+
+        setState(() {
+          _products = fetchedProducts;
+        });
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error al cargar los productos')),
+        );
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getProducts();
   }
 
   @override
@@ -24,8 +79,8 @@ class _HomeView extends State<HomeView> {
     return Scaffold(
       backgroundColor: Colors.blueGrey[900],
       appBar: AppBar(
-        title: Text(
-          "Premios CELO",
+        title: const Text(
+          "INVENTARIO",
           style: TextStyle(
             fontSize: 28,
             fontWeight: FontWeight.bold,
@@ -33,140 +88,129 @@ class _HomeView extends State<HomeView> {
           ),
         ),
         backgroundColor: Colors.blueGrey[800],
-        iconTheme: IconThemeData(color: Colors.white),
+        iconTheme: const IconThemeData(color: Colors.white),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: CustomButtonSanti(
+              icon: Icons.upload,
+              label: "Tienda",
+              onPressed: () {
+              },
+              backgroundColor: Color(0xFF00C4FF),
+              foregroundColor: Colors.black,
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+              borderRadius: 12.0,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child:CustomButtonSanti(
+              icon: Icons.upload,
+              label: "Clima",
+              onPressed: () {
+
+              },
+              backgroundColor: Color(0xFF00C4FF),
+              foregroundColor: Colors.black,
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+              borderRadius: 12.0,
+            ),
+          ),
+        ],
       ),
-      body: Center(
+      drawer: CustomDrawer(),
+      body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const Text(
-                "Hola Mundo",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+              // Alternar entre vista en lista o en celdas
+              isGridView
+                  ? GridView.builder(
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, // 2 columnas en la cuadrícula
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
                 ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                "Soy Pedro",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white70,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                visible ? "Que te den" : "ADIOS",
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 20,
-                  color: Colors.lightGreenAccent,
-                ),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                "CONTADOR DE CLICKS",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.red,
-                ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                "$clickContador",
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 30,
-                  color: Colors.blue,
-                ),
-              ),
-              const SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text("btn1"),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.lightBlueAccent, // Cambia aquí
-                      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                itemCount: _products.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Colors.blueGrey[800],
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(Icons.shopping_cart, color: Colors.white),
+                          Text(
+                            _products[index].name,
+                            style: const TextStyle(
+                                color: Colors.white, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'Precio: ${_products[index].price} - Tienda: ${_products[index].store}',
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 10),
-                  IconButton(
-                    icon: const FaIcon(FontAwesomeIcons.anchor),
-                    color: Colors.lightBlueAccent,
-                    onPressed: () {
-                      clickBtnContador();
-                      print("VALOR DEL CONTADOR: $clickContador");
-                    },
-                  ),
-                  const SizedBox(width: 10),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text("btn2"),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.lightBlueAccent, // Cambia aquí
-                      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                  );
+                },
+              )
+                  : ListView.builder(
+                shrinkWrap: true,
+                itemCount: _products.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                      _products[index].name,
+                      style: const TextStyle(color: Colors.white),
                     ),
-                  ),
-                ],
+                    subtitle: Text(
+                      'Precio: ${_products[index].price} - Tienda: ${_products[index].store}',
+                      style: const TextStyle(color: Colors.white70),
+                    ),
+                    leading: const Icon(Icons.shopping_cart, color: Colors.white),
+                  );
+                },
               ),
             ],
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            // Cambiar entre vista en lista y vista en celdas
+            isGridView = !isGridView;
+          });
+        },
         backgroundColor: Colors.lightBlueAccent,
-        child: const FaIcon(FontAwesomeIcons.apple),
+        child: Icon(isGridView ? Icons.view_list : Icons.grid_view),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.blueGrey[800],
+        unselectedItemColor: Colors.white,
+        backgroundColor: Colors.blueGrey[900],
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Buscar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Configuración',
+          ),
+        ],
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-// Antiguo o para gente con muchos principios
-Widget pintadoComoJava(){
-  Text saludo=Text("HOLA MUNDO");
-  Text saludo2=Text("SOY SANTI");
-  Text saludo3=Text("WELCOME TO HELL HDP");
-  TextButton Btn1=TextButton(onPressed: (){}, child: Text("btn1"));
-  TextButton Btn2=TextButton(onPressed: (){}, child: Text("btn2"));
-
-  // Primero declara la fila porque es como JAVA, y es medio Tonto
-
-  Row fila = Row(children: [Btn1, Btn2],mainAxisAlignment: MainAxisAlignment.center);
-
-  Column columna=Column(children: [saludo,saludo2,saludo3,fila]);
-
-  return columna;
-}
-*/

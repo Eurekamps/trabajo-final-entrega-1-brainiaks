580579be131884bfd0ebb5214a5a737b38edebfb
nickv <nickvilca6@gmail.com>
2024-12-11 19:51:57 +0100

    creacion de la vista para comunidades

---
+++ lib/Views/CommunityView.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CommunutyView extends StatefulWidget{
+  @override
+  State<CommunutyView> createState() => _CommunutyViewState();
+}
+
+class _CommunutyViewState extends State<CommunutyView> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}

ab60b843a0bd86a12f4b0606c1ecacff44f9e56f
nickv <nickvilca6@gmail.com>
2025-04-06 23:21:06 +0200

    se agrega un campo mas para los tags

--- lib/FBObjects/FBPost.dart
+++ lib/FBObjects/FBPost.dart
@@ -1,21 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FBPost {
-  String texto; // Contenido del texto de la publicación
-  String? imagenURL; // URL de la imagen asociada (opcional)
-  DateTime fechaCreacion; // Fecha y hora de la creación de la publicación
-  final String autorID; // Identificador único del autor de la publicación
-  final String autorApodo; // Apodo del autor de la publicación (nuevo campo)
+  String texto;
+  String? imagenURL;
+  DateTime fechaCreacion;
+  final String autorID;
+  final String autorApodo;
+  final List<String> tags;
+  //final int comentariosCount;
 
   FBPost({
     required this.texto,
     this.imagenURL,
     required this.fechaCreacion,
     required this.autorID,
-    required this.autorApodo, // Nuevo parámetro requerido
+    required this.autorApodo,
+    this.tags = const [],
+    //this.comentariosCount = 0,
   });
 
-  /// Constructor para crear una instancia de FBPost desde un documento Firestore
   factory FBPost.fromFirestore(
       DocumentSnapshot<Map<String, dynamic>> snapshot,
       SnapshotOptions? options,
@@ -26,18 +29,21 @@ class FBPost {
       imagenURL: data?['imagenURL'],
       fechaCreacion: (data?['fechaCreacion'] as Timestamp).toDate(),
       autorID: data?['autorID'] ?? '',
-      autorApodo: data?['autorApodo'] ?? 'Anónimo', // Nuevo campo
+      autorApodo: data?['autorApodo'] ?? 'Anónimo',
+      tags: List<String>.from(data?['tags'] ?? []),
+      //comentariosCount: data?['comentariosCount'] ?? 0,
     );
   }
 
-  /// Método para convertir una instancia de FBPost a un mapa compatible con Firestore
   Map<String, dynamic> toFirestore() {
     return {
       'texto': texto,
       'imagenURL': imagenURL,
       'fechaCreacion': fechaCreacion,
       'autorID': autorID,
-      'autorApodo': autorApodo, // Nuevo campo en el mapa
+      'autorApodo': autorApodo,
+      'tags': tags,
+      //'comentariosCount': comentariosCount,
     };
   }
 }
